01d3023670f368cdd4bec3fe62b87f7fa79d6cb5
Charles Clark <charles.t.clark89@gmail.com>
2022-02-12 16:33:40 -0600

    Updated testing suite with new model additions

--- blog/tests/test_post.py
+++ blog/tests/test_post.py
@@ -31,3 +31,20 @@ def test_default_posted_time_is_set():
     assert post.id
     assert post.posted_at is not None
     assert post.posted_at.date() == datetime.datetime.now().date()
+
+@pytest.mark.django_db
+def test_default_viewed_count_is_set():
+    post = Post(title="Test", text="Testing")
+    post.save()
+
+    assert post.id
+    assert post.viewed_count == 0
+
+@pytest.mark.django_db
+def test_increment_viewed_count_is_valid():
+    post = Post(title="Test", text="Testing")
+    post.viewed_count += 1
+    post.save()
+
+    assert post.id
+    assert post.viewed_count == 1
