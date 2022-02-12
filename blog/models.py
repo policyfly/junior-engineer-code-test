7cd1a4620ea2a7321bc59b93fb42779f24b73440
Charles Clark <charles.t.clark89@gmail.com>
2022-02-12 16:16:21 -0600

    Fixed bug in blog posted_at and added migrations

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,13 @@ import datetime
 from django.db import models
 
 def auto_now():
-    return datetime.datetime.now() - datetime.timedelta(days=1)
+    return datetime.datetime.now()
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
     text = models.TextField(max_length=2048)
     posted_by = models.CharField(max_length=50, default="root")
-    posted_at = models.DateTimeField(default=auto_now)
+    posted_at = models.DateTimeField(auto_now_add=True)
     edited_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
