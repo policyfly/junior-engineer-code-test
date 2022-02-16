007cc67d7f46cbfb8906380e5b3a86838bffe853
Hayden Chudy <hjc1710@gmail.com>
2022-02-16 12:43:08 -0500

    [js] Clearer JavaScript prompt

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 /**
- * Prompt: Implement a function that validates that the input Vehicles have the
- * following shape and all numbers will be sensible integers.
- * 
+ * Prompt: Implement a function that validates that the input Vehicles
+ * have the expected minimum @param expectedYear and total cost is
+ * @param expectedCost. Do not include vehicles with type `Trailer` in
+ * these calculations.
+ * You can assume the following shape of Vehicles below and that all
+ * numbers will be sensible integers.
  * interface Vehicle {
  *   type: 'Truck' | 'Trailer' | 'Tractor'
  *   year: number
  *   value: number
  * }
- * 
- * @param {Vehicle[]} input 
+ *
+ * @param {Vehicle[]} input
  * @param {number} expectedCost The expected sum cost of all vehicles passed in
  * @param {number} expectedOldestYear The expected minimum year of all vehicles passed in
  * @return {boolean} A boolean indicating whether input matches expected{Cost,Year}
